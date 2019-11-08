fe4057353b8f7efd8dff0e78687b36e4a0e909c3
Jodi Rhoades <jo_kawgirl@yahoo.com>
2019-11-08 11:41:10 -0500

    Updated css and button styling

--- Assets/code.js
+++ Assets/code.js
@@ -17,74 +17,54 @@ function startquiz() {
 }
 var timeInterval
 var questionPostion = 0
-var timeLeft = questions.length*15
+var timeLeft = questions.length * 15
 var domTime = document.getElementById('timer')
-function writeButton(){
-    document.getElementById('actualQuestion').textContent= questions[questionPostion].title
+function writeButton() {
+    document.getElementById('actualQuestion').textContent = questions[questionPostion].title
     for (let i = 0; i < 4; i++) {
-        var currentButton =document.getElementById('btn'+i)
+        var currentButton = document.getElementById('btn' + i)
         currentButton.textContent = questions[questionPostion].choices[i]
-        currentButton.onclick = function (){clicked(questions[questionPostion].choices[i])}
+        currentButton.onclick = function () { clicked(questions[questionPostion].choices[i]) }
     }
 }
 
-function clicked(answer){
+function clicked(answer) {
     console.log(answer)
-    if(answer === questions[questionPostion].answer){
+    if (answer === questions[questionPostion].answer) {
         console.log('correct')
     }
-    else{
+    else {
         console.log('wrong')
         timeLeft = timeLeft - 10
     }
     questionPostion++
-    if (questionPostion===questions.length){
+    if (questionPostion === questions.length) {
         clearInterval(timeInterval)
-        console.log('your score= '+timeLeft)
+        console.log('your score= ' + timeLeft)
         endGame()
     }
-    else{
+    else {
         writeButton()
     }
-    
-
 }
+
 function timer() {
-    
-  timeInterval = setInterval(function() {
-    timeLeft--;
-    domTime.textContent = 'Seconds left: '+timeLeft
-    if (timeLeft === 0) {
-        clearInterval(timeInterval)
-      console.log('out of time')
-    }
+    timeInterval = setInterval(function () {
+        timeLeft--;
+        domTime.textContent = 'Seconds left: ' + timeLeft
+        if (timeLeft === 0) {
+            clearInterval(timeInterval)
+            console.log('out of time')
+        }
 
-  }, 1000);
+    }, 1000);
 }
-function endGame(){
-    document.getElementById('actualQuestion').innerHTML = '<h2>Game Over</h2><br> Your score: '+timeLeft
-    document.getElementById('answerBtns').innerHTML = 'Enter Intials <input type="text" name="fname"> <button id="submitIntials" type="button"> submit</button>'
+function endGame() {
+    document.getElementById('actualQuestion').innerHTML = '<h2>All Done!</h2><br> Your final score: ' + timeLeft 
+    document.getElementById('answerBtns').innerHTML = 'Enter Initials <input type="text" name="initials"> <button id="submitInitials" type="button"> submit</button>'
 }
-//startBtn.addEventListener("click", setTime());
 
-
-
-
-    // function setTime() {
-
-    //     var time = 75
-    //     var timerInterval = setInterval(countdown, 1000);
-    //     timeEl.textContent = time;
-
-    //     function countdown() {
-    //         time--;
-    //         timeEl.textContent = time;
-
-    //         if (time === -1) {
-    //             alert("Time is UP!")
-    //         };
-
-
-    //         if else()
-
-    //     }
+#submitInitials {
+    font-family: sans-serif;
+    margin: ;
+}
--- Assets/questions.js
+++ Assets/questions.js
@@ -3,32 +3,32 @@
 var questions = [
     {
         title: "What special character is used at the end of a function?",
-        choices: [":", "'", "/", "}"],
+        choices: ["1.  :", "2.  '", "3.  /", "4.  }"],
         answer: "}"
     },
     {
         title: "A 'confirm' statement gives you a ______ result.",
-        choices: ["string", "array", "boolean", "square brackets"],
+        choices: ["1.  string", "2.  array", "3.  boolean", "4.  square brackets"],
         answer: "boolean"
     },
-    // {
-    //     title: "In this for loop, if you wanted add one each loop through you would use what in the blank?: for (var i = 0; i < vegetables.length; ___ )",
-    //     choices: ["i++", "+=", "/1", "==="],
-    //     answer: "i++"
-    // },
-    // {
-    //     title: "In this array: var animals = [dogs, horses, cows, pigs]; which one is the index of [4]? ",
-    //     choices: ["dogs", "cows", "pigs", "none of the above"],
-    //     answer: "none of the above"
-    // },
-    // {
-    //     title: "There are four parts to a box model.  Which one is missing: Margin, padding, content?",
-    //     choices: ["size", "border", "position", "width"],
-    //     answer: "border"
-    // },
+    {
+        title: "In this for loop, if you wanted add one each loop through you would use what in the blank?: for (var i = 0; i < vegetables.length; ___ )",
+        choices: ["1.  i++", "2.  +=", "3.  /1", "4.  ==="],
+        answer: "i++"
+    },
+    {
+        title: "In this array: var animals = [dogs, horses, cows, pigs]; which one is the index of [4]? ",
+        choices: ["1.  dogs", "2.  cows", "3.  pigs", "4.  none of the above"],
+        answer: "none of the above"
+    },
+    {
+        title: "There are four parts to a box model.  Which one is missing: Margin, padding, content?",
+        choices: ["1.  size", "2.  border", "3.  position", "4.  width"],
+        answer: "border"
+    },
 
 ];
 
 console.log(questions);
- 
+
 
